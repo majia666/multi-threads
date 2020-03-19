d7c0ef1ac21a0a4c59f5476d9234f92cbaed3060
majia <614640021@qq.com>
2020-03-19 09:25:20 +0800

    多线程学习

---
+++ src/main/java/com/majia/designpattern/guardedsuspension/LivenessException.java
@@ -0,0 +1,8 @@
+package com.majia.designpattern.guardedsuspension;
+
+public class LivenessException extends RuntimeException {
+
+    public LivenessException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/majia/designpattern/guardedsuspension/RequestQueue.java
+++ src/main/java/com/majia/designpattern/guardedsuspension/RequestQueue.java
@@ -6,14 +6,35 @@ public class RequestQueue {
 
     private final LinkedList<Request> queue = new LinkedList<>();
 
+    // 超时时间30秒
+    private final static Long TIMEOUT = 3000L;
+
     public Request getRequest() {
-        synchronized (queue) {
+       /* synchronized (queue) {
             while (queue.size() <= 0) {
                 try {
                     queue.wait();
                 } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                    break;
+                    return null;
+                }
+            }
+            return queue.removeFirst();
+        }*/
+
+        // 30秒守护线程的条件还没有到 就抛出超时异常
+        synchronized (queue) {
+            long start_time = System.currentTimeMillis(); // 开始时间
+
+            while (queue.size() <= 0) {
+                long now = System.currentTimeMillis(); // 当前时间
+                long reset = TIMEOUT - (now - start_time);
+
+                if (reset <=0){
+                    throw new LivenessException("TIME OUT");
+                }
+                try {
+                    queue.wait(reset);
+                } catch (InterruptedException e) {
                     return null;
                 }
             }
